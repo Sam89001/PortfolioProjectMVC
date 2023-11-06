fe5f0633d181b96f3e6f55897394f4779bb4f3f8
Samuel Cole <Samscreativespace@gmail.com>
2023-11-06 19:11:02 +0000

    javscript hover animation added to first images. will need optimisation and probably a state as this will not work as intended.

--- PortfolioProjectMVC/wwwroot/js/site.js
+++ PortfolioProjectMVC/wwwroot/js/site.js
@@ -1,4 +1,22 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your JavaScript code.
+
+
+projectImages = document.getElementById("project-images");
+topImage = document.getElementById("top-image");
+bottomImage = document.getElementById("bottom-image");
+
+projectImages.addEventListener('mouseenter', function () {
+    topImage.style.transformOrigin = 'left bottom'; // Set the origin to the bottom left corner
+    topImage.style.transform = 'rotate(-10deg)';
+
+    bottomImage.style.transformOrigin = 'left bottom'; // Set the origin to the bottom left corner
+    bottomImage.style.transform = 'rotate(10deg)';
+});
+
+projectImages.addEventListener('mouseleave', function () {
+    topImage.style.transform = 'rotate(0deg)'; // Reset to original position
+    bottomImage.style.transform = 'rotate(0deg)'; // Reset to original position
+});
+
